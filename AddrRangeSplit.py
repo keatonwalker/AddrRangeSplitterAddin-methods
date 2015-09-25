302090c9508a3b4debf108bb632bb818d9805f01
kwalker9 <klwalker9@gmail.com>
2015-09-24 18:09:56 -0600

    Methods were prepared for addin.

--- AddrRangeSplit.py
+++ AddrRangeSplit.py
@@ -3,8 +3,8 @@ Created on Aug 22, 2015
 
 @author: kwalker
 '''
-import arcpy, math
-
+import arcpy, math, time
+print "imports"
 class WholeRoad(object):
     
     def __init__(self, lineGeometry, L_F_Add, L_T_Add, R_F_Add, R_T_Add):
@@ -158,16 +158,101 @@ class SplitRoad(WholeRoad):
         pass
         
     
+class Config(object):
+    
+    def __init__ (self):
+        self.srcFieldNames = []
+        self.srcRow = None
+        self.geometryField = "SHAPE@"
+        self.leftFromField = "L_F_ADD" 
+        self.leftToField = "L_T_ADD"
+        self.rightFromField = "R_F_ADD"
+        self.rightToField = "R_T_ADD"
         
+    def getFieldIndex(self, fieldName):
+        return self.srcFieldNames.index(fieldName)
+    
+    def cleanSrcRow(self):
+        shapeIndex = self.getFieldIndex('Shape')
+        del self.srcFieldNames[shapeIndex]
+        del self.srcRow[shapeIndex]
+        
+        shapeLenIndex = self.getFieldIndex('Shape_Length')
+        del self.srcFieldNames[shapeLenIndex]
+        del self.srcRow[shapeLenIndex]
+        
+    
+    def createInsertRow(self, lineGeometry, leftFrom, leftTo, rightFrom, rightTo):
+        print "geofield index: {}".format(self.getFieldIndex(self.geometryField))
+        insertRow = list(self.srcRow)
+        insertRow[self.getFieldIndex(self.geometryField)] = lineGeometry 
+        insertRow[self.getFieldIndex(self.leftFromField)] = leftFrom 
+        insertRow[self.getFieldIndex(self.leftToField)] = leftTo
+        insertRow[self.getFieldIndex(self.rightFromField)] = rightFrom
+        insertRow[self.getFieldIndex(self.rightToField)] = rightTo
+        
+        return insertRow
+    
+    def deleteRoadById (self, id, inFc):
+        layer = "testing"
+        arcpy.MakeFeatureLayer_management (inFc, layer)
+        arcpy.SelectLayerByAttribute_management (layer, "NEW_SELECTION", "OBJECTID = {}".format(id))
+        arcpy.DeleteFeatures_management(layer)
+    
     
 if __name__ == "__main__":
 
-    inFc = r"C:\KW_Working\AddrRangeSplitAddin\AddRangeSplit.gdb\SingleTestRoad"
-    fieldNames = ["SHAPE@", "L_F_ADD", "L_T_ADD", "R_F_ADD", "R_T_ADD"]
+    inFc = "C:\GIS\Work\AddRangeSplit.gdb\SingleTestRoad"
+    fieldNames = ["SHAPE@", "L_F_ADD", "L_T_ADD", "R_F_ADD", "R_T_ADD", "OID@", "*"]
+    configUtil = Config()
+    wholeRoad = None
+    startSideRoad = None 
+    endSideRoad = None
     with arcpy.da.SearchCursor(inFc, fieldNames, explode_to_points = False) as cursor:
         for row in cursor:
-            print row
-            r = WholeRoad(row[0], row[1], row[2], row[3], row[4])
-            startSideRoad, endSideRoad = r.getStartAndEndSideRoads(428705.77, 4332011.096)
+            configUtil.srcFieldNames = list(cursor.fields)
+            configUtil.srcRow = list(row)
+            configUtil.cleanSrcRow()
+            wholeRoad = WholeRoad(row[configUtil.getFieldIndex("SHAPE@")], 
+                          row[configUtil.getFieldIndex("L_F_ADD")], 
+                          row[configUtil.getFieldIndex("L_T_ADD")], 
+                          row[configUtil.getFieldIndex("R_F_ADD")], 
+                          row[configUtil.getFieldIndex("R_T_ADD")])
+            
+            wholeRoad.setId(row[configUtil.getFieldIndex("OID@")])
+            break
+    
+    splitTime = time.time()
+    startSideRoad, endSideRoad = wholeRoad.getStartAndEndSideRoads(428705.77, 4332011.096)
+    print"split time: {}".format(time.time() - splitTime)
+#     print wholeRoad.lineGeometry.spatialReference.factoryCode
+#     print endSideRoad.lineGeometry.spatialReference.factoryCode
+    
+    insTime = time.time()    
+    insCursor = arcpy.da.InsertCursor(inFc, configUtil.srcFieldNames)
+    startSideRow = configUtil.createInsertRow(startSideRoad.lineGeometry, 
+                                                 startSideRoad.leftFromAddr, 
+                                                 startSideRoad.leftToAddr, 
+                                                 startSideRoad.rightFromAddr, 
+                                                 startSideRoad.rightToAddr)
+    startSideId = insCursor.insertRow(startSideRow)
+    
+    endSideRow = configUtil.createInsertRow(endSideRoad.lineGeometry, 
+                                                 endSideRoad.leftFromAddr, 
+                                                 endSideRoad.leftToAddr, 
+                                                 endSideRoad.rightFromAddr, 
+                                                 endSideRoad.rightToAddr)
+    print endSideRow
+    endSideId = insCursor.insertRow(endSideRow)
+    del insCursor
+    
+    #configUtil.deleteRoadById(wholeRoad.id, inFc)
+    
+    print"ins time: {}".format(time.time() - insTime)
+    
+    print "Start ID: {}, End ID: {}".format(startSideId, endSideId)
+    
+    
+    
             
             
